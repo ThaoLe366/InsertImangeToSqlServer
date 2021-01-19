0dc19e8074beb187bda4fa988335f29ab2c5927f
VERMOUNTH <46836329+VERMOUNTH@users.noreply.github.com>
2021-01-19 11:14:39 +0700

    project add image type to sql server

---
+++ GetImageForProduct/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace GetImageForProduct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdProduct = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dtgvListProduct = new System.Windows.Forms.DataGridView();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListProduct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // txtIdProduct
+            // 
+            this.txtIdProduct.Location = new System.Drawing.Point(89, 28);
+            this.txtIdProduct.Name = "txtIdProduct";
+            this.txtIdProduct.Size = new System.Drawing.Size(528, 22);
+            this.txtIdProduct.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(670, 28);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(542, 241);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dtgvListProduct
+            // 
+            this.dtgvListProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvListProduct.Location = new System.Drawing.Point(19, 288);
+            this.dtgvListProduct.Name = "dtgvListProduct";
+            this.dtgvListProduct.RowHeadersWidth = 51;
+            this.dtgvListProduct.RowTemplate.Height = 24;
+            this.dtgvListProduct.Size = new System.Drawing.Size(756, 150);
+            this.dtgvListProduct.TabIndex = 6;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(89, 74);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(528, 161);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.dtgvListProduct);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtIdProduct);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListProduct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdProduct;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dtgvListProduct;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ GetImageForProduct/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetImageForProduct
+{
+ 
+    public partial class Form1 : Form
+    {
+        string ConnStr = @"Data Source=localhost;Initial Catalog=BOOKSTOREWEB;Integrated Security=True";
+        SqlConnection conn = null;
+  
+        SqlCommand cmd;
+        string imageURL = null;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(ConnStr);
+            cmd = conn.CreateCommand();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    imageURL = ofd.FileName;
+                    pictureBox.Image= Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(imageURL);
+            byte[] arr;
+            ImageConverter converter = new ImageConverter();
+            arr = (byte[])converter.ConvertTo(img, typeof(byte[]));
+          try
+          {
+                int idProduct = Convert.ToInt32(txtIdProduct.Text);
+                if (idProduct > 0)
+                {
+                using (SqlConnection openCon = new SqlConnection(ConnStr))
+                {
+                    string saveStaff = "INSERT into PhotoProduct(idProduct, photo) values (@idProduct, @image)";
+
+                    using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                    {
+                        querySaveStaff.Connection = openCon;
+                        querySaveStaff.Parameters.AddWithValue("@idProduct", idProduct);
+                        querySaveStaff.Parameters.AddWithValue("@image", arr);
+                        
+                        openCon.Open();
+
+                        querySaveStaff.ExecuteNonQuery();
+                    }
+                }
+
+                using (SqlConnection openCon = new SqlConnection(ConnStr))
+                {
+                    string saveStaff = "select * from PhotoProduct";
+
+                    using (SqlCommand querySaveStaff = new SqlCommand(saveStaff))
+                    {
+                        querySaveStaff.Connection = openCon;
+                       
+                        openCon.Open();
+
+             
+                        DataTable dt = new DataTable();
+                        dt.Load(querySaveStaff.ExecuteReader());
+                        dtgvListProduct.DataSource = dt;
+                    }
+                }
+
+               }
+          }
+           catch
+          {
+              MessageBox.Show("You are missing out something!!!");
+          }
+           
+
+        }
+    }
+}
